08f08841e0f0b036ab1c396296d24599e35367b2
susana <susanabrancodias@gmail.com>
2020-05-14 23:06:54 +0100

    initial import

---
+++ consumebike/src/main/java/com/consume/bike/consumebike/ConsumebikeApplication.java
@@ -0,0 +1,13 @@
+package com.consume.bike.consumebike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumebikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumebikeApplication.class, args);
+	}
+
+}
